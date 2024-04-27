acdc15f7a085aa01cc297bbb6520d1bac0be8c40
Isabel Torres <isabel0729.it@gmail.com>
2024-04-27 18:26:47 +0200

    modificacion scr

--- DBpg.js
+++ DBpg.js
@@ -3,20 +3,18 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
 
-  user: 'itt',
-  host: 'dpg-coij7sf79t8c738hu5c0-a',
-  database: 'clinicaoftalmologica_itt',
-  password: 'RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP',
-  port: 5432
+  // user: 'itt',
+  // host: 'dpg-coij7sf79t8c738hu5c0-a',
+  // database: 'clinicaoftalmologica_itt',
+  // password: 'RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP',
+  // port: 5432
   // connectionString: 'postgres://itt:RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP@dpg-coij7sf79t8c738hu5c0-a.oregon-postgres.render.com/clinicaoftalmologica_itt'
 
-
-
-  // user: 'postgres',
-  // host: 'localhost',
-  // database: 'Clinica_oftalmologica',
-  // password: 'Basededatos20',
-  // port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Clinica_oftalmologica',
+  password: 'Basededatos20',
+  port: 5432,
   // connectionString: 'postgres://itt:RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP@dpg-coij7sf79t8c738hu5c0-a.oregon-postgres.render.com/clinicaoftalmologica_itt'
 
 
