59a258d233946e72c06ff7a06ce9518c89c7f456
Isabel Torres <isabel0729.it@gmail.com>
2024-04-27 21:15:41 +0200

    configuracion de la conexion de la base de datos render

--- DBpg.js
+++ DBpg.js
@@ -1,30 +1,24 @@
-//import pg from 'pg'
+//import { config } from 'dotenv'
+//config()
+
 const Pool = require('pg').Pool
-
 const pool = new Pool({
-
-  // user: 'itt',
-  // host: 'dpg-coij7sf79t8c738hu5c0-a',
-  // database: 'clinicaoftalmologica_itt',
-  // password: 'RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP',
-  // port: 5432
-  // connectionString: 'postgres://itt:RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP@dpg-coij7sf79t8c738hu5c0-a.oregon-postgres.render.com/clinicaoftalmologica_itt'
-
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Clinica_oftalmologica',
-  password: 'Basededatos20',
+  //External URL de render
+  user: 'itt',
+  host: 'dpg-coij7sf79t8c738hu5c0-a.oregon-postgres.render.com',
+  database: 'clinicaoftalmologica_itt',
+  password: 'RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP',
   port: 5432,
+  ssl: true
+
   // connectionString: 'postgres://itt:RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP@dpg-coij7sf79t8c738hu5c0-a.oregon-postgres.render.com/clinicaoftalmologica_itt'
 
-
-
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // port: process.env.PORT,
-  // database: process.env.DB_NAME,
-  //connectionString: process.env.DATABASE_URL,
+  // En Local: 
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'Clinica_oftalmologica',
+  // password: 'Basededatos20',
+  // port: 5432,
 
 
 });
