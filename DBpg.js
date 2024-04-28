b332d77735f6032835fc9a40229b1f1ca06d4b56
Isabel Torres <isabel0729.it@gmail.com>
2024-04-28 07:51:12 +0200

    modificacion db3

--- DBpg.js
+++ DBpg.js
@@ -3,21 +3,16 @@
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  //External URL de render
-  user: 'itt',
-  host: 'dpg-coij7sf79t8c738hu5c0-a.oregon-postgres.render.com',
-  database: 'clinicaoftalmologica_itt',
-  password: 'RBL6Ndnn7sfWmoaX9omVIllhhKBDmwPP',
-  port: 5432,
-  ssl: true,
-  connectionString: process.env.DATABASE_URL
 
-  // En Local: 
-  // user: 'postgres',
-  // host: 'localhost',
-  // database: 'Clinica_oftalmologica',
-  // password: 'Basededatos20',
-  // port: 5432,
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASS: process.env.DB_PASS,
+  DB_PORT: process.env.DB_PORT,
+  //External URL de render
+  DATABASE_URL: process.env.DATABASE_URL,
+  ssl: true,
+
 
 
 });
