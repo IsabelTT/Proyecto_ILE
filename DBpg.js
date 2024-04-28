9e8061d7eb915334088368d1b6c8da9dea78de89
Isabel Torres <isabel0729.it@gmail.com>
2024-04-28 11:32:44 +0200

    ancho de formulario

--- DBpg.js
+++ DBpg.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-  //External URL de render
+  //Internal URL de render
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 
