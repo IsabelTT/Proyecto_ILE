1189f035e4c1fc1033fd4afc82bfaad8d8eb328d
Isabel Torres <isabel0729.it@gmail.com>
2024-04-28 09:47:49 +0200

    modificacion db6

--- DBpg.js
+++ DBpg.js
@@ -4,13 +4,13 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
 
-  DB_USER: process.env.DB_USER,
-  DB_HOST: process.env.DB_HOST,
-  DB_NAME: process.env.DB_NAME,
-  DB_PASS: process.env.DB_PASS,
-  DB_PORT: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  hostname: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
   //External URL de render
-  DATABASE_URL: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   //ssl: true,
 
 
